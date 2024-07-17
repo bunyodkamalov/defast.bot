d3a0e0d564cbd8ce107e742853421c0300df3196
Bunyod <kbunyod3011@gmail.com>
2024-07-17 12:32:45 +0500

    Add Akt Sverka

---
+++ Defast.Bot.Infrastructure/EventHandlers/To'lovlar/AktSverka/HandleAktSverkaOneMonthPeriod.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using AutoMapper;
+using Defast.Bot.Application.Common;
+using Defast.Bot.Domain.Enums;
+using Defast.Bot.Infrastructure.DTOs;
+using Defast.Bot.Infrastructure.EventHandlers.ReplyKeyboardMarkups;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Defast.Bot.Infrastructure.EventHandlers.To_lovlar.AktSverka;
+
+public class HandleOneMonthPeriod(IBusinessPartnerService businessPartnerService, IMapper mapper)
+{
+    public async ValueTask HandleAsync( ITelegramBotClient telegramBotClient, CallbackQuery callbackQuery, ELanguage language, CancellationToken cancellationToken)
+    {
+        var startDate = DateOnly.FromDateTime(DateTime.Today.AddMonths(-1)).ToString("dd.MM.yyyy");
+        var endDate = DateOnly.FromDateTime(DateTime.Today).ToString("dd.MM.yyyy");
+        
+        var bp = await businessPartnerService.GetByTgIdAsync(callbackQuery.Message!.Chat.Id, cancellationToken);
+
+        var businessPartner = await businessPartnerService.GetByCardCodeAsync(bp.CardCode, startDate, endDate, cancellationToken);
+
+        if (businessPartner is not null)
+        {
+            var data = mapper.Map<BusinessPartnerDto>(businessPartner);
+            await telegramBotClient.SendTextMessageAsync(callbackQuery.Message!.Chat.Id, 
+                $"Ism: {businessPartner.CardName}\n" +
+                $"Telefon Raqam: {businessPartner.MobilePhone}\n\n" +
+                $"Davr boshidagi qoldiq:   {data.BalanceFirstDayOfTheMonth.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')} $\n" +
+                $"Sotib olingan mol:   {data.PurchasedProduct.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')} $ \n" +
+                $"To'langan pul:   {data.PaidMoney.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')} $ \n" +
+                $"Jami to'lanishi kerak:   {data.BalanceLastDayOfTheMonth.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')} $ \n" +
+                $"Jami qarzdorlik:   {data.CurrentAccountBalance.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')}     $ \n" +
+                $"Sotib olingan jami \u33a1 :   {data.TotalAmountReceived.ToString("00.00", CultureInfo.InvariantCulture).Replace(',', ' ')} \u33a1  \n" +
+                $"Pul aylanishi: {data.MoneySpeed}\n\n" +
+                $"Davr: {startDate} - {endDate}",
+                replyMarkup: UserMainMenuMarkup.Get(language),
+                cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ Defast.Bot.Infrastructure/EventHandlers/To'lovlar/AktSverka/HandleAktSverkaPeriods.cs
@@ -0,0 +1,38 @@
+﻿using Defast.Bot.Domain.Enums;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Defast.Bot.Infrastructure.EventHandlers.To_lovlar.AktSverka;
+
+public static class HandlePeriods
+{
+    public static async ValueTask HandleAsync(ITelegramBotClient telegramBotClient, CallbackQuery callbackQuery, ELanguage language, CancellationToken cancellationToken)
+    {
+        var inlineKeyboardMarkup = new InlineKeyboardMarkup(
+            new[]
+            {
+                [
+                    InlineKeyboardButton.WithCallbackData(language == ELanguage.Uzbek ? "Bir kunlik" : "За неделю",
+                        $"aktSverkaOneDayPeriod")
+                ],
+                [
+                    InlineKeyboardButton.WithCallbackData(language == ELanguage.Uzbek ? "Bir haftalik" : "За неделю",
+                        $"aktSverkaOneWeekPeriod")
+                ],
+                [
+                    InlineKeyboardButton.WithCallbackData(language == ELanguage.Uzbek ? "Bir oylik" : "За месяцу",
+                        $"aktSverkaOneMonthPeriod")
+                ],
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData(
+                        language == ELanguage.Uzbek ? "Hamma vaqt davomida" : "За все время", $"aktSverkaAllTimePeriod")
+                }
+            });
+
+        await telegramBotClient.SendTextMessageAsync(callbackQuery.Message!.Chat.Id,
+            language == ELanguage.Uzbek ? "Akt sverkani ko'rish uchun vaqt kesimini tanlang" : "Выберите период для просмотра акт сверки",
+            replyMarkup: inlineKeyboardMarkup, cancellationToken: cancellationToken);
+    }
+}
